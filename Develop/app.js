aedf0d9809e7ba714cccedece50b08fb55610795
Mario Kozic <kun.zizinho@gmail.com>
2020-05-08 03:40:06 -0400

    added function for building team

--- Develop/app.js
+++ Develop/app.js
@@ -4,16 +4,21 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const jest = require("jest");
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
+
+
 const output = path.resolve(__dirname, "output", "team.html");
-​
-​
+// const outputPath = path.join(OUTPUT_DIR, "./output/team.html");
+// const render = require("./lib/htmlRenderer");​​
+const render = require("./lib/htmlRenderer");
+
+
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 let employees = [];
+
+// otvorili smo funkciju kojom cemo kreirati menadera
+//inquirerom smo zatrazili korisnikov input
+//zatim smo svojstvom validate i arrow funkcijom definirali da ako korisnik ne utipka nista da mu stigne poruka da 
+// stavi barem 1 karakter
 
 function createMngr() {
     inquirer.prompt([{
@@ -75,16 +80,10 @@ function createMngr() {
         createTeam();
     })
 
+    //liniji 73 
 
 }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+
 function createTeam() {
     inquirer.prompt([{
         type: "list",
@@ -112,6 +111,7 @@ function createTeam() {
         }
     })
 }
+
 function createIntern() {
     console.log("intern")
     inquirer.prompt([{
@@ -178,11 +178,7 @@ function createIntern() {
         createTeam();
     })
 }
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
+
 function createEngineer() {
     console.log("engineer")
     inquirer.prompt([{
@@ -250,11 +246,7 @@ function createEngineer() {
     })
 
 }
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
 
 function buildTeam() {
     fs.writeFileSync(output, render(employees), "utf8")
